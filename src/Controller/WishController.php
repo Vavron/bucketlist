826a7991f63faec11b4a78c49fb87ed721d3838d
Krystor <victor.avron2020@campus-eni.fr>
2021-01-28 14:19:59 +0100

    Module 6

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Form\AddWishType;
+use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Repository\RepositoryFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,17 +18,61 @@ class WishController extends AbstractController
     /**
      * @Route("/wishes", name="wish_list")
      */
-    public function list(): Response
+    public function list(WishRepository $repo): Response
     {
-        return $this->render('wish/list.html.twig', []);
+        $wishes = $repo->findBy(
+            ["isPublished" => true], //clauses where
+            ["dateCreated" => "DESC"] //order by
+        );
+        return $this->render('wish/list.html.twig', ["wishes" => $wishes]);
     }
 
     /**
      * @Route("/wishes/detail/{id}", name="wish_detail")
      */
-    public function detail($id): Response
+    public function detail($id, WishRepository $wishRepository): Response
     {
-        return $this->render('wish/detail.html.twig', ['id' => $id]);
+        $wish=$wishRepository->find($id);
+
+        if (!$wish){
+            //alors on déclenche une 404
+            throw $this->createNotFoundException('This wish is gone.');
+        }
+
+
+
+        return $this->render('wish/detail.html.twig', [
+            //passe l'id présent dans l'URL à twig
+            "wish_id" => $id,
+            "wish" => $wish
+        ]);
+    }
+
+    /**
+     * @Route("/wishes/create", name="wish_add")
+     */
+    public function create(Request $request, EntityManagerInterface $entityManager) : Response
+    {
+        $wish = new Wish();
+
+        $form =$this->createForm(AddWishType::class, $wish);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $wish->setDateCreated(new \DateTime());
+            $wish->setIsPublished(true);
+
+            $entityManager->persist($wish);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre voeu a bien a bien été jeté.');
+
+            return $this->redirectToRoute('wish_detail' , ["id" => $wish->getId()]);
+        }
+
+        return $this->render('wish/add_wish.html.twig', ["wish_form" => $form->createView()]);
     }
 
 
---
+++ src/Form/AddWishType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Wish;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddWishType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description', TextareaType::class)
+            ->add('author', \Symfony\Component\Form\Extension\Core\Type\TextType::class)
+            ->add('submit', SubmitType::class, ["label"=>"Throw !"])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Wish::class,
+        ]);
+    }
+}
